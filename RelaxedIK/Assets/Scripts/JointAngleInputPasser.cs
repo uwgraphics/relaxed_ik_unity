748b25a2e5e5e8fa33d1d12b168c7e0870f30607
Haochen Shi <hshi74@wisc.edu>
2020-07-01 23:05:48 -0500

    Fix some issues of the GUI

--- RelaxedIK/Assets/Scripts/JointAngleInputPasser.cs
+++ RelaxedIK/Assets/Scripts/JointAngleInputPasser.cs
@@ -5,32 +5,34 @@ using UnityEngine.UI;
 
 namespace RosSharp.RosBridgeClient
 {
+    public class InputPasser
+    {
+        public Slider slider;
+        public Text text;
+        public JointStateWriter writer;
+        public bool updated;
+    }
+
     public class JointAngleInputPasser : MonoBehaviour
     {
-        public GameObject panel;
         public GameObject hGroupPrefab;
         public GameObject textPrefab;
+        public GameObject labelPrefab;
         public GameObject sliderPrefab;
-        public float maxJointVelocity;
 
-        private List<JoyAxisJointTransformWriter> writers;
-        [SerializeField]
-        private List<Slider> sliders;
+        private List<InputPasser> passers;
 
         // Start is called before the first frame update
         void Start()
         {
-            writers = new List<JoyAxisJointTransformWriter>();
-            sliders = new List<Slider>();
+            passers = new List<InputPasser>();
 
             RelaxedIKUnity r = FindObjectOfType(typeof(RelaxedIKUnity)) as RelaxedIKUnity;
             foreach (GameObject link in r.robotLinks)
             {
-                JoyAxisJointTransformWriter writer = link.AddComponent(typeof(JoyAxisJointTransformWriter)) as JoyAxisJointTransformWriter;
-                writer.MaxVelocity = maxJointVelocity;
-                writers.Add(writer);
+                InputPasser passer = new InputPasser();
 
-                GameObject hGroup = Instantiate(hGroupPrefab, panel.transform) as GameObject;
+                GameObject hGroup = Instantiate(hGroupPrefab, transform) as GameObject;
                 hGroup.name = "panel_" + link.name;
                 GameObject textObj = Instantiate(textPrefab, hGroup.transform) as GameObject;
                 textObj.name = "text_" + link.name;
@@ -38,26 +40,36 @@ namespace RosSharp.RosBridgeClient
 
                 GameObject sliderObj = Instantiate(sliderPrefab, hGroup.transform) as GameObject;
                 sliderObj.name = "slider_" + link.name;
-                Slider slider = sliderObj.GetComponent<Slider>();
-                sliderObj.GetComponentInChildren<Text>().text = slider.value.ToString("0.0");
-                slider.onValueChanged.AddListener(
-                    delegate {
-                        sliderObj.GetComponentInChildren<Text>().text = slider.value.ToString("0.0");
-                        writer.Write(slider.value);
-                    }
-                );
-                sliders.Add(slider);
+                passer.slider = sliderObj.GetComponent<Slider>();
+
+                HingeJointLimitsManager jointManager = link.GetComponent<HingeJointLimitsManager>();
+                passer.slider.value = jointManager.AngleActual * Mathf.Deg2Rad;
+                passer.slider.maxValue = jointManager.LargeAngleLimitMax * Mathf.Deg2Rad;
+                passer.slider.minValue = jointManager.LargeAngleLimitMin * Mathf.Deg2Rad;
+                passer.slider.onValueChanged.AddListener(delegate {passer.updated = true;});
+
+                GameObject labelObj = Instantiate(labelPrefab, hGroup.transform) as GameObject;
+                labelObj.name = "label_" + link.name;
+                passer.text = labelObj.GetComponent<Text>();
+                passer.text.text = passer.slider.value.ToString("0.0");
+
+                passer.writer = link.AddComponent(typeof(JointStateWriter)) as JointStateWriter;
+
+                passers.Add(passer);
             }
         }
 
         // Update is called once per frame
         void Update()
         {
-            
-        }
-
-        public void SliderOnInput() {
-
+            foreach (InputPasser passer in passers) 
+            {
+                if (passer.updated) {
+                    passer.text.text = passer.slider.value.ToString("0.0");
+                    passer.writer.Write(passer.slider.value);
+                    passer.updated = false;
+                }
+            }
         }
     }
 }
